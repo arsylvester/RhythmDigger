f7b25bb09668efbf55b2a5b12ac4306e10ea94a6
Jeremiah <jeremiah.plauche@gmail.com>
2021-12-19 15:37:29 -0600

    Resolved deltas

--- Assets/Scripts/Beat Controller/beatMover.cs
+++ Assets/Scripts/Beat Controller/beatMover.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class beatMover : MonoBehaviour
+public class BeatMover : MonoBehaviour
 {
     private Vector3 startLocation;
     [SerializeField]
